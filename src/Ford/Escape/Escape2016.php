78c62148c27c8491bb83cfc29e6463ea6db82c14
hujianreplace <285520463@qq.com>
2016-11-08 22:12:12 +0800

    aaa

---
+++ src/Ford/Escape/Escape2016.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Ford\Escape;
+
+class Escape2016
+{
+    public static function info()
+    {
+        echo "This is Ford Escape2016!<br />";
+    }
+}
---
+++ src/Ford/Focus/Focus2016.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Ford\Focus;
+class Focus2016
+{
+    public function info()
+    {
+        echo "This is Ford Focus2016!<br>";
+    }
+}
